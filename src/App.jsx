ce4f74cd11b7c6dee7231a548b926f5654ea73dd
Kavi20021216 <kaveetharandili@gmail.com>
2025-08-12 12:55:07 -0700

    created login page✏️

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/registerPage";
 import AdminPage from "./pages/adminPage";
+import TestPage from "./pages/testPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<div className="w-full h-screen flex justify-center items-center">
-				
+				    <Toaster position="top-right"/>
 					<Routes path="/">
 						<Route path="/" element={<HomePage/>}/>
 						<Route path="/login" element={<LoginPage/>}/>
+						<Route path="/test" element={<TestPage/>}/>
 						<Route path="/register" element={<RegisterPage/>}/>
 						<Route path="/admin/*" element={<AdminPage/>}/>
 					</Routes>
